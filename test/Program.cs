700b82d1e2911613eda538c1550926c9c16e725e
D1X3R0NCH1K <artemkul310507@gmail.com>
2024-10-20 10:59:56 +0300

    Добавьте файлы проекта.

---
+++ test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Как вас зовут? ");
+            string name = Console.ReadLine();
+
+            Console.Write("Сколько вам лет? ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Вас зовут {name} и ваш возраст {age}");
+
+            Console.Write("Введите вашу дату рождения: ");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine($"Ваша дата рождения: {birthDate}");
+            Console.ReadKey();
+        }
+    }
+}
